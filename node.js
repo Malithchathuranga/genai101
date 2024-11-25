78a82e4e00e2d16961fe6efad0d4538fe698c95f
Malithchathuranga <malithchathu2002@gmail.com>
2024-11-25 15:53:37 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,5 +1,5 @@
 const facts = [
-    "I enjoy photography and have captured stunning landscapes during my travels.",
+"I enjoy photography and have captured stunning landscapes during my travels.",
 "I love cooking and experiment with new recipes every weekend.",
 "I’ve been to 10 countries and am always planning my next adventure.",
 "I’m passionate about gardening and grow a variety of herbs and vegetables.",
